aa92e2a6ae7c9d550808bf9aa142b4ce5fa1245f
AkashGadge1809 <akashgadge0918@gmail.com>
2024-12-22 00:46:25 +0530

    Save changes before Angular update

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reactive-form',
+  standalone:true,
   imports: [],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
--- src/app/components/template-form/template-form.component.ts
+++ src/app/components/template-form/template-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-template-form',
+  standalone:true,
   imports: [],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
